efd2dcf5a555eda0dd650ddc28219e7ceffb2bd9
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2024-12-12 20:51:21 +0200

    add style

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,32 @@ document.addEventListener("DOMContentLoaded", function () {
 	accordionFunction();
 	addAnimationInit();
 	galleryTabs();
-
+	animationHeader();
 });
 AOS.init({
 	duration: 1000,
 });
+const animationHeader = () =>{
+	let lastScrollTop = 0;
+
+  window.addEventListener("scroll", function () {
+		const headerNav = document.querySelector(".header__bottom");
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		let windowInnerWidth = window.innerWidth;
+    if (windowInnerWidth >= 1024) {
+      if (scrollTop > lastScrollTop) {
+        if (scrollTop > 100) {
+          headerNav.classList.add("fixed-header-nav");
+          headerNav.style.animationName = "smoothScroll";
+        }
+      } else if (scrollTop <= 0) {
+        headerNav.classList.remove("fixed-header-nav");
+        headerNav.style.animationName = "removeSmoothScroll";
+      }
+      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+    }
+  });
+}
 const toggleMenu = () =>{
 	const htmlElement = document.querySelector("html");
 	const burgerMenu = document.querySelector(".burger");
